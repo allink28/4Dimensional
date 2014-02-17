4bdb5f1641033bf0790544575d980e0ccb3ff2c8
Allen <allink28@gmail.com>
2014-02-17 17:48:11 -0500

    Implemented timer/location functionality

--- src/com/github/allink28/android_gps_buildexample/MainActivity.java
+++ src/com/github/allink28/android_gps_buildexample/MainActivity.java
@@ -1,5 +1,6 @@
 package com.github.allink28.android_gps_buildexample;
 
+import java.text.DecimalFormat;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
@@ -24,38 +25,44 @@ import android.widget.TextView;
 
 public class MainActivity extends Activity implements LocationListener {
   public static SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("h:mm a EEE, MMM d");      
-  EditText startTimeTB;
-  EditText startLatTB, startLongTB;
+  EditText startTimeTB, endTimeTB;
+  EditText startLatTB, startLongTB, startAltTB;
   EditText currentLat, currentLong, currentAlt;
+  EditText endLatTB, endLongTB, endAltTB;
+  TextView summaryTV;
   Button start, mark;
   private long startTime = 0, endTime = 0;
   private static String START = "Start", STOP = "Stop";
   LocationManager locationManager;
-  Location currentLocation;
+  Location currentLocation, startLocation, endLocation;
   
-  Intent intent;
-  PendingIntent pendingIntent;
   NotificationManager notificationManager;
-  private static int NOTIFICATION_ID = 0;
+  private static final int NOTIFICATION_ID = 0;
   
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+    init();
+    locationManager = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
+    locationManager.requestLocationUpdates("gps", 1000 * 2, 1, this); //update after 1000ms of a move of 1m
+  }
+  
+  private void init(){
     startTimeTB = (EditText) this.findViewById(R.id.startTime);
     startLatTB = (EditText) this.findViewById(R.id.startLatitude);
-    startLongTB = (EditText) this.findViewById(R.id.startLongitude);    
+    startLongTB = (EditText) this.findViewById(R.id.startLongitude);  
+    startAltTB = (EditText) this.findViewById(R.id.startAlt);
     currentLat = (EditText) this.findViewById(R.id.currentLat);
     currentLong = (EditText) this.findViewById(R.id.currentLong);
     currentAlt = (EditText) this.findViewById(R.id.currentAlt);
-    
-    intent = new Intent(getApplicationContext(), MainActivity.class);
-    pendingIntent = PendingIntent.getActivity(getApplicationContext(), 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
-    notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-    
+    endTimeTB = (EditText) this.findViewById(R.id.endTime);
+    endLatTB = (EditText) this.findViewById(R.id.endLat);
+    endLongTB = (EditText) this.findViewById(R.id.endLong);
+    endAltTB = (EditText) this.findViewById(R.id.endAlt);   
+    summaryTV = (TextView) this.findViewById(R.id.summary);
     start = (Button) this.findViewById(R.id.start_button);
-    locationManager = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
-    locationManager.requestLocationUpdates("gps", 1000 * 3, 1, this); //update after 1000ms of a move of 1m
+    notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
   }
 
   @Override
@@ -73,9 +80,7 @@ public class MainActivity extends Activity implements LocationListener {
         ", speed: "+location.getSpeed());    
 //    location.distanceTo(dest)
 //  Date d = new Date(location.getTime());
-    currentLat.setText(formatCoordinates(location.getLatitude()));
-    currentLong.setText(formatCoordinates(location.getLongitude()));
-    currentAlt.setText(String.valueOf( Math.round(location.getAltitude())) );    
+    setLocationDisplay(currentLocation, currentLat, currentLong, currentAlt);
   }
   
   public void toggleTimer(View v){
@@ -90,15 +95,36 @@ public class MainActivity extends Activity implements LocationListener {
   }
   
   public void startTimer(View v) {
+    startLocation = currentLocation;
     startTime = System.currentTimeMillis();
     Date d = new Date(startTime);
-    String formattedDate =DATE_FORMAT.format(d); 
+    String formattedDate = DATE_FORMAT.format(d); 
     startTimeTB.setText(formattedDate);
+    setLocationDisplay(startLocation, startLatTB, startLongTB, startAltTB);
+    clearLocationDisplay();
     setNotification(formattedDate);
+  }
+  
+  public void stopTimer(View v) {    
+    endTime = System.currentTimeMillis();        
+    long elapsedTime = endTime - startTime;    
+    endTimeTB.setText(DATE_FORMAT.format(new Date(endTime)));
+    endLocation = currentLocation;
+    setLocationDisplay(endLocation, endLatTB, endLongTB, endAltTB);
+    int seconds = (int) (elapsedTime/1000);
+    int minutes = seconds/60;
+    seconds = seconds%60;
+    final DecimalFormat timeFormat = new DecimalFormat("*0##");
+//    summaryTV.setText("Time: "+minutes/60 +":"+minutes%60+":"+seconds);
+    summaryTV.setText("Time: "+minutes/60 +":"+timeFormat.format(minutes%60)+":"+timeFormat.format(seconds));
+    notificationManager.cancel(NOTIFICATION_ID);
   }
   
   @SuppressWarnings("deprecation")
   public void setNotification(String startDate){
+    Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+    intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP);
+    PendingIntent pendingIntent = PendingIntent.getActivity(getApplicationContext(), 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);    
     Notification notification = new Notification();
     notification.icon = R.drawable.ic_launcher;
     notification.tickerText = "Timer running. Started at: "+startDate;
@@ -116,22 +142,25 @@ public class MainActivity extends Activity implements LocationListener {
         NOTIFICATION_ID, notification);    
   }
   
-  
-  
-  public void stopTimer(View v) {    
-    endTime = System.currentTimeMillis();
-    long elapsedTime = endTime - startTime;
-    Log.i("stopTimer", "elapsedTime: "+elapsedTime +" ms");
-    notificationManager.cancel(NOTIFICATION_ID);
-  }
-  
-  private String formatCoordinates(double coordinate){
-    return String.valueOf( ((int)(coordinate*100000))/100000.0);
-  }
-  
   public void mark(View v){
     
   }
+  
+  private void setLocationDisplay(Location l, EditText lat, EditText lon, EditText alt){
+    if (l != null) {
+      lat.setText(String.valueOf( ((int)(l.getLatitude()*100000))/100000.0));
+      lon.setText(String.valueOf( ((int)(l.getLongitude()*100000))/100000.0));
+      alt.setText(String.valueOf( Math.round(l.getAltitude())));
+    }
+  }
+  private void clearLocationDisplay(){    
+    String emptyString = "";
+    endTimeTB.setText(emptyString);
+    endLatTB.setText(emptyString);
+    endLongTB.setText(emptyString);
+    endAltTB.setText(emptyString);
+  }
+  
 
   @Override
   public void onProviderDisabled(String provider) {
