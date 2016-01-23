57695e42b2bf7dc09c51865122bf2d41cc009b1c
Allen <allink28@gmail.com>
2016-01-23 18:38:40 -0500

    Migrated to Android Studios

--- gen/com/github/allink28/android_gps_buildexample/BuildConfig.java
+++ gen/com/github/allink28/android_gps_buildexample/BuildConfig.java
@@ -1,6 +1,8 @@
-/** Automatically generated file. DO NOT MODIFY */
+/*___Generated_by_IDEA___*/
+
 package com.github.allink28.android_gps_buildexample;
 
+/* This stub is only used by the IDE. It is NOT the BuildConfig class actually packed into the APK */
 public final class BuildConfig {
-    public final static boolean DEBUG = true;
+  public final static boolean DEBUG = Boolean.parseBoolean(null);
 }
--- src/com/github/allink28/FourDimensional/Converter.java
+++ src/com/github/allink28/FourDimensional/Converter.java
@@ -1,15 +1,15 @@
 /**
- * 
+ *
  */
 package com.github.allink28.FourDimensional;
 
 import java.text.DecimalFormat;
 
 /**
- * This class is for unit conversions that otherwise clutter up the 
+ * This class is for unit conversions that otherwise clutter up the
  * MainActivity
- * @author Allen Preville
  *
+ * @author Allen Preville
  */
 public class Converter {
   private static final DecimalFormat TIME_FORMAT = new DecimalFormat("*0##");
@@ -18,36 +18,37 @@ public class Converter {
 
   /**
    * @param distance Distance in meters, given by GPS
-   * @return Formated distance and unit
+   * @return Formatted distance and unit
    */
-  public static String formatDistance(float distance, boolean useMiles){
+  public static String formatDistance(float distance, boolean useMiles) {
     if (useMiles) {
       return metersToMiles(distance);
     } else {
-      if (distance < 1000){ //Less than 1 kilometer
-        return "~"+Math.round(distance)+" m";
+      if (distance < 1000) { //Less than 1 kilometer
+        return "~" + Math.round(distance) + " m";
       }
-      if (distance < 100000){ //Less than 100 kilometers 
-        return TWO_DECIMAL_PLACES.format(distance/1000.0) +" km";
+      if (distance < 100000) { //Less than 100 kilometers
+        return TWO_DECIMAL_PLACES.format(distance / 1000.0) + " km";
       }
-      return ONE_DECIMAL_PLACE.format(distance/1000)+" km";
+      return ONE_DECIMAL_PLACE.format(distance / 1000) + " km";
     }
   }
-  
+
   /**
    * mi = km * 0.62137
-   * @return "x.xx mi" 
+   *
+   * @return "x.xx mi"
    */
-  private static String metersToMiles(float meters) {    
+  private static String metersToMiles(float meters) {
     return TWO_DECIMAL_PLACES.format(meters * .62137 / 1000.0) + " mi";
   }
-  
-  
-  public static String formatTime(long time){    
-    int seconds = (int) time/1000;
-    int hours = seconds/(3600);
+
+
+  public static String formatTime(long time) {
+    int seconds = (int) time / 1000;
+    int hours = seconds / (3600);
     seconds -= hours * 3600;
-    int minutes = seconds/60;
+    int minutes = seconds / 60;
     seconds -= minutes * 60;
     if (time <= (1000 * 60)) { //If total time is less than a minute
       return seconds + " seconds";
@@ -55,39 +56,41 @@ public class Converter {
     if (time <= (1000 * 60 * 60)) { //If total time is less than an hour
       return TIME_FORMAT.format(minutes) + ":" + TIME_FORMAT.format(seconds);
     }
-    return TIME_FORMAT.format(hours) + ":" + TIME_FORMAT.format(minutes) + 
-        ":" + TIME_FORMAT.format(seconds);
+    return TIME_FORMAT.format(hours) + ":" + TIME_FORMAT.format(minutes) +
+            ":" + TIME_FORMAT.format(seconds);
   }
 
-  public static String formatCoordinate(double coordinate, boolean useDMS){
+  public static String formatCoordinate(double coordinate, boolean useDMS) {
     if (useDMS) {
       return decimalDegreesToDMS(coordinate);
     }
-    return ((int)(coordinate*100000))/100000.0  +"°";
+    return ((int) (coordinate * 100000)) / 100000.0 + "°";
   }
-  
-//The whole units of degrees will remain the same (i.e. in 121.135° longitude, start with 121°).
-//Multiply the decimal by 60 (i.e. .135 * 60 = 8.1).
-//The whole number becomes the minutes (8').
-//Take the remaining decimal and multiply by 60. (i.e. .1 * 60 = 6).
-//The resulting number becomes the seconds (6"). Seconds can remain as a decimal.
-//Take your three sets of numbers and put them together, using the symbols for degrees (°), minutes (‘), and seconds (") (i.e. 121°8'6" longitude)
-  //examples: 121.135 degrees == 121° 8' 6"
-  //          51.477222 degrees == 51° 28' 37.9986"
-  private static String decimalDegreesToDMS(double coordinate){
+
+  /**
+   * The whole units of degrees will remain the same (i.e. in 121.135° longitude, start with 121°).
+   * Multiply the decimal by 60 (i.e. .135 * 60 = 8.1).
+   * The whole number becomes the minutes (8').
+   * Take the remaining decimal and multiply by 60. (i.e. .1 * 60 = 6).
+   * The resulting number becomes the seconds (6"). Seconds can remain as a decimal.
+   * Take your three sets of numbers and put them together, using the symbols for degrees (°), minutes (°), and seconds (") (i.e. 121°8'6" longitude)
+   * examples: 121.135 degrees == 121° 8' 6"
+   * 51.477222 degrees == 51° 28' 37.9986"
+   */
+  private static String decimalDegreesToDMS(double coordinate) {
     String[] dms = String.valueOf(coordinate).split("\\.");
-    String decimal = (dms.length == 1 || dms[1].length() == 0)? "0" : dms[1];
-    coordinate = Double.valueOf("."+decimal) * 60;
+    String decimal = (dms.length == 1 || dms[1].length() == 0) ? "0" : dms[1];
+    coordinate = Double.valueOf("." + decimal) * 60;
     int minutes = (int) coordinate;
     double seconds = (coordinate - minutes) * 60;
-    
-    return dms[0]+"° " + minutes+"' " + Math.round(seconds*10)/10.0+"\""; 
+
+    return dms[0] + "° " + minutes + "' " + Math.round(seconds * 10) / 10.0 + "\"";
   }
- 
-  
+
+
 //  public static String metricToImperial(double distance){
 //   TODO 
 //  }
-  
+
 
 }
