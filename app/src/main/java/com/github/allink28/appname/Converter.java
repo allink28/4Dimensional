f500bf860f9f98e21e2e1673b6efd9d2b9131d0d
Allen <allink28@gmail.com>
2016-02-28 13:17:37 -0500

    Renamed package

---
+++ app/src/main/java/com/github/allink28/appname/Converter.java
@@ -0,0 +1,95 @@
+/**
+ *
+ */
+package com.github.allink28.FourDimensional;
+
+import java.text.DecimalFormat;
+
+/**
+ * This class is for unit conversions that otherwise clutter up the
+ * MainActivity
+ *
+ * @author Allen Preville
+ */
+public class Converter {
+    private static final DecimalFormat TIME_FORMAT = new DecimalFormat("*0##");
+    private static final DecimalFormat TWO_DECIMAL_PLACES = new DecimalFormat("#.##");
+    private static final DecimalFormat ONE_DECIMAL_PLACE = new DecimalFormat("#.#");
+
+    /**
+     * @param distance Distance in meters, given by GPS
+     * @return Formatted distance and unit
+     */
+    public static String formatDistance(float distance, boolean useMiles) {
+        if (useMiles) {
+            return metersToMiles(distance);
+        } else {
+            if (distance < 1000) { //Less than 1 kilometer
+                return "~" + Math.round(distance) + " m";
+            }
+            if (distance < 100000) { //Less than 100 kilometers
+                return TWO_DECIMAL_PLACES.format(distance / 1000.0) + " km";
+            }
+            return ONE_DECIMAL_PLACE.format(distance / 1000) + " km";
+        }
+    }
+
+    /**
+     * mi = km * 0.62137
+     *
+     * @return "x.xx mi"
+     */
+    private static String metersToMiles(float meters) {
+        return TWO_DECIMAL_PLACES.format(meters * .62137 / 1000.0) + " mi";
+    }
+
+
+    public static String formatTime(long time) {
+        int seconds = (int) time / 1000;
+        int hours = seconds / (3600);
+        seconds -= hours * 3600;
+        int minutes = seconds / 60;
+        seconds -= minutes * 60;
+        if (time <= (1000 * 60)) { //If total time is less than a minute
+            return seconds + " seconds";
+        }
+        if (time <= (1000 * 60 * 60)) { //If total time is less than an hour
+            return TIME_FORMAT.format(minutes) + ":" + TIME_FORMAT.format(seconds);
+        }
+        return TIME_FORMAT.format(hours) + ":" + TIME_FORMAT.format(minutes) +
+                ":" + TIME_FORMAT.format(seconds);
+    }
+
+    public static String formatCoordinate(double coordinate, boolean useDMS) {
+        if (useDMS) {
+            return decimalDegreesToDMS(coordinate);
+        }
+        return ((int) (coordinate * 100000)) / 100000.0 + "\u00B0";
+    }
+
+    /**
+     * The whole units of degrees will remain the same (i.e. in 121.135° longitude, start with 121°).
+     * Multiply the decimal by 60 (i.e. .135 * 60 = 8.1).
+     * The whole number becomes the minutes (8').
+     * Take the remaining decimal and multiply by 60. (i.e. .1 * 60 = 6).
+     * The resulting number becomes the seconds (6"). Seconds can remain as a decimal.
+     * Take your three sets of numbers and put them together, using the symbols for degrees (°), minutes ('), and seconds (") (i.e. 121°8'6" longitude)
+     * examples: 121.135 degrees == 121° 8' 6"
+     * 51.477222 degrees == 51° 28' 37.9986"
+     */
+    private static String decimalDegreesToDMS(double coordinate) {
+        String[] dms = String.valueOf(coordinate).split("\\.");
+        String decimal = (dms.length == 1 || dms[1].length() == 0) ? "0" : dms[1];
+        coordinate = Double.valueOf("." + decimal) * 60;
+        int minutes = (int) coordinate;
+        double seconds = (coordinate - minutes) * 60;
+
+        return dms[0] + "\u00B0 " + minutes + "' " + Math.round(seconds * 10) / 10.0 + "\"";
+    }
+
+
+//  public static String metricToImperial(double distance){
+//   TODO 
+//  }
+
+}
---
+++ app/src/main/java/com/github/allink28/appname/MainActivity.java
@@ -0,0 +1,235 @@
+package com.github.allink28.FourDimensional;
+
+import java.text.DateFormat;
+import java.util.Date;
+
+import com.github.allink28.android_gps_buildexample.R;
+
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.app.Activity;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.ToggleButton;
+
+public class MainActivity extends Activity implements LocationListener {
+    public static final DateFormat DATE_FORMAT = DateFormat.getDateInstance(); // new SimpleDateFormat("h:mm a EEE, MMM d");
+    private EditText startTimeTB, endTimeTB;
+    private EditText startLatTB, startLongTB;
+    private EditText currentLat, currentLong;
+    private EditText endLatTB, endLongTB;
+    private TextView summaryTV;
+    private ToggleButton start;
+    private long startTime = 0, endTime = 0;
+    private float distance;
+    private SharedPreferences settings;
+    private LocationManager locationManager;
+    private Location currentLocation, startLocation, endLocation;
+
+    private NotificationManager notificationManager;
+    private static final int NOTIFICATION_ID = 0;
+
+    // -------------- Activity Lifecycle methods --------------------------------
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        settings = PreferenceManager.getDefaultSharedPreferences(this.getApplicationContext());
+        init();
+    }
+
+    private void init() {
+        startTimeTB = (EditText) this.findViewById(R.id.startTime);
+        startLatTB = (EditText) this.findViewById(R.id.startLatitude);
+        startLongTB = (EditText) this.findViewById(R.id.startLongitude);
+        currentLat = (EditText) this.findViewById(R.id.currentLat);
+        currentLong = (EditText) this.findViewById(R.id.currentLong);
+        endTimeTB = (EditText) this.findViewById(R.id.endTime);
+        endLatTB = (EditText) this.findViewById(R.id.endLat);
+        endLongTB = (EditText) this.findViewById(R.id.endLong);
+        summaryTV = (TextView) this.findViewById(R.id.summary);
+        start = (ToggleButton) this.findViewById(R.id.start_button);
+
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        try {
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,
+                    1000 * Integer.valueOf(settings.getString(getString(R.string.update_time), "5")),//ms to update after
+                    Integer.valueOf(settings.getString(getString(R.string.update_dist), "3")),//meters to update after
+                    this);
+        } catch (SecurityException se) {
+            Log.d("MainActivity.onResume", "SecurityException onResume: " + se);
+        }
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+                Intent intent = new Intent(this, SettingsActivity.class);
+                startActivity(intent);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    // ----------------------- Activity Methods ---------------------------------
+
+    @Override
+    public void onLocationChanged(Location newLocation) {
+        if (start.isChecked() && newLocation != null && currentLocation != null) {
+            distance += newLocation.distanceTo(currentLocation);
+            boolean useMiles = settings.getBoolean(getString(R.string.useMiles), false);
+            summaryTV.setText("Distance traveled: " + Converter.formatDistance(distance, useMiles));
+        }
+        currentLocation = newLocation;
+        setLocationDisplay(currentLocation, currentLat, currentLong);
+    }
+
+    /**
+     * Executes the correct action when the start/stop button is pressed,
+     * and resets values when the action is finished.
+     */
+    public void toggleTimer(View v) {
+        if (start.isChecked()) {
+            startTimer();
+            endTime = 0;
+            distance = 0;
+        } else {
+            stopTimer();
+            startTime = 0;
+        }
+    }
+
+    private void startTimer() {
+        startLocation = currentLocation;
+        startTime = System.currentTimeMillis();
+        Date d = new Date(startTime);
+        String formattedDate = DATE_FORMAT.format(d);
+        startTimeTB.setText(formattedDate);
+        setLocationDisplay(startLocation, startLatTB, startLongTB);
+        clearLocationDisplay();
+        setNotification(formattedDate);
+    }
+
+    private void stopTimer() {
+        endTime = System.currentTimeMillis();
+        endTimeTB.setText(DATE_FORMAT.format(new Date(endTime)));
+        endLocation = currentLocation;
+        notificationManager.cancel(NOTIFICATION_ID);
+        setLocationDisplay(endLocation, endLatTB, endLongTB);
+        setLocationDisplay(startLocation, startLatTB, startLongTB);
+
+
+        StringBuilder sb = new StringBuilder();
+        boolean useMiles = settings.getBoolean(getString(R.string.useMiles), false);
+        if (distance != 0) {
+            sb.append("Distance traveled: ").append(Converter.formatDistance(distance, useMiles)).append("\n");
+        }
+        sb.append("Time: ").append(Converter.formatTime(endTime - startTime));
+        if (startLocation != null && endLocation != null) {
+            sb.append("\nDisplacement: ").append(Converter.formatDistance(startLocation.distanceTo(endLocation), useMiles));
+        }
+        summaryTV.setText(sb.toString());
+    }
+
+    @SuppressWarnings("deprecation")
+    public void setNotification(String startDate) {
+        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP);
+        PendingIntent pendingIntent = PendingIntent.getActivity(getApplicationContext(), 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+        Notification notification = new Notification();
+        notification.icon = R.drawable.ic_launcher;
+        notification.tickerText = "Timer running. Started at: " + startDate;
+        notification.number = 0;
+        notification.setLatestEventInfo(getApplicationContext(), //api 14 compatible
+                "4Dimensional - Timer running",
+                "Started at: " + startDate,
+                pendingIntent);
+//    Notification notification = new Notification.Builder(getApplicationContext()) //for api 16
+//          .setContentTitle("Content title")
+//          .setContentText("content text") //setSmallIcon(R.drawable.new_email).setLargeIcon(aBitmap)
+//          .setContentIntent(pendingIntent)
+//          .build();
+        notificationManager.notify(NOTIFICATION_ID, notification);
+    }
+
+    /**
+     * Save the current coordinate
+     */
+    public void mark(View v) {
+        //TODO save coordinate
+        Toast.makeText(MainActivity.this, R.string.marked, Toast.LENGTH_SHORT).show();
+    }
+
+
+    // ------------------------- Helper methods ---------------------------------
+
+    private void setLocationDisplay(Location l, EditText lat, EditText lon) {
+        if (l != null) {
+            boolean useDMS = settings.getBoolean(getString(R.string.useDMS), false);
+            lat.setText(String.valueOf(Converter.formatCoordinate(l.getLatitude(), useDMS)));
+            lon.setText(String.valueOf(Converter.formatCoordinate(l.getLongitude(), useDMS)));
+        } else {
+            lat.setText(null);
+            lon.setText(null);
+        }
+    }
+
+    private void clearLocationDisplay() {
+        String emptyString = "";
+        summaryTV.setText(emptyString);
+        endTimeTB.setText(emptyString);
+        endLatTB.setText(emptyString);
+        endLongTB.setText(emptyString);
+    }
+
+
+    // ------------------------ Unimplemented Methods ---------------------------
+
+    @Override
+    public void onProviderDisabled(String provider) {
+        // TODO: blank out current location?
+        Toast.makeText(MainActivity.this, String.format(getResources().getString(R.string.providerDisabled), (provider != null) ? provider.toUpperCase() : null ), Toast.LENGTH_LONG).show();
+        Log.i("onProviderDisabled", "onProviderDisabled(" + provider + ") not yet implemented");
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+        // TODO Auto-generated method stub
+        Log.i("onProviderEnabled", "onProviderEnabled(" + provider + ") not yet implemented");
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+        // TODO Auto-generated method stub
+        Log.i("onStatusChanged", "onStatusChanged(" + provider + ", " + status + ", " + extras + ") not yet implemented");
+    }
+
+}
